f8bb8e5b9b0e7df60a99e4df2c6bb2ed2a292eab
yanek-skv <75174290+yanek-skv@users.noreply.github.com>
2020-11-28 19:05:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+#ctrl+?
+# a='hello'
+# b='world'
+
+# print(a + b)
+# print(a * 3)
+z='my name yanek'
+# print(z[0])
+# print(z[-1])
+
+#name[start:stop(not open):step]
+
+# print(z[0:6])
+# print(z[:6])
+# print(z[7:])
+# print(z[::])
+# print(z[::2])
+# print(z[::-1])
+
+# print(z[::-2])
+# print(len(z))
+# print (z.lower())
+# print (z.upper())
+
+
+# print (z.islower())
+# print (z.isupper())
+
+
+
+# x='my name yanek yanek'
+
+# print(x.find('yanek'))
+#
+
+# print(x.replace('yanek','Yanek'))
+#
+
+#
+# print(x.split('å'))
+#
+
+#
+# print(x.index('å'))
+
+
+
+v='yanek'
+g=41
+
+print(f'my name {v} i am {g} age')
+
+
+
+print(ord('a'))
+
+
+# a=input('input a ')
+# b=input('input b ')
+# print('len str 1 < str 2', len(a) < len(b) )
+# print(ord(a[0]), ord((b[0])))
+# print('Ascci str 1 < str 2', a < b)
+
+
+s=input('inpot str')
+print(s.swapcase())
+
+
+e=input('input ')
+print(e.count('l'))
+
+
+
+h=input('input ')
+#hl=h.lower()
+print(h.lower() == h.lower()[::-1])
+
+
+
+f=input('input ctr')
+print('3 numer',f[2])
+print('mirror',f[-1])
+print('not last simvol',f[:-1])
+print('vhet', f[0::2])
+print('not chet', f[1::2])
+
